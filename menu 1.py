402e4ec6cfdedd8642897f93b8336a80226f27f4
thiago <155448340+thiago23sl@users.noreply.github.com>
2025-04-08 19:48:59 -0300

    Update menu.py
    Remoção de repetição do código:
    
    No código 3, o mesmo bloco inteiro estava duplicado. Agora foi corrigido, mantendo apenas uma versão do menu.
    
    Melhoria na organização e estrutura:
    
    Código mais limpo, com comentários explicativos e bem estruturado.
    
    Uso correto do while True: com break para controlar a saída do programa.
    
    Validação de lista vazia ao listar nomes:
    
    Agora, ao escolher a opção "Listar", o sistema verifica se existem estudantes cadastrados
    
    Correção de ortografia e acentuação:
    
    Corrigido: "diciplinas" → "disciplinas"
    
    Corrigido: "excuindo" → "excluindo"
    
    Corrigido: "não a estudantes" → "não há estudantes"
    
    Ajustes de indentação e variáveis:
    
    A variável nome está declarada corretamente no início e sendo manipulada dentro do menu de estudantes.
    
    Foco maior no CRUD de estudantes:
    
    Ainda que "professores", "disciplinas", etc. estejam listados no menu principal, apenas estudantes têm ações implementadas até agora (incluir, listar).

--- menu 1.py
+++ menu 1.py
@@ -1,120 +1,84 @@
-# primeiro menu basico  para eu ir em determinados menus
-OpcaoPrimeiroMenu = 0
-nome= []
+#thiago silva lima
+#Analise e desenvolvimento de sistemas 
 
-while OpcaoPrimeiroMenu != 9:
+# Inicializa uma lista vazia para armazenar nomes
+nome = []
+
+# Loop principal do programa
+while True:
     print('AGUARDE ENQUANTO O MENU É INCIADO')
-    print("-" * 15+'MENU PRINCIPAL'+"-" * 15)
-    OpcaoPrimeiroMenu = int(input('(1) Gerenciar estudantes.' 
-                     '\n(2) Gerenciar professores.'
-                     '\n(3) Gerenciar diciplinas'
-                     '\n(4) Gerenciar turmas'
-                     '\n(5) Gerenciar matricula'
-                     '\n(9) Sair'
-                     '\nInforme a opção desejada a seguir:'))
-    if OpcaoPrimeiroMenu == 1:
-        escrito= 'ESTUDANTES'
-    elif OpcaoPrimeiroMenu == 2:
-        escrito= 'PROFESSORES'
-    elif OpcaoPrimeiroMenu == 3:
-        escrito= 'DICIPLINAS'
-    elif OpcaoPrimeiroMenu == 4:
-        escrito= 'TURMAS'
-    elif OpcaoPrimeiroMenu == 5:
-        escrito= 'MATRICULAS'
-    elif OpcaoPrimeiroMenu == 9:
+    print("-" * 15 + 'MENU PRINCIPAL' + "-" * 15)
+    
+    # Exibe opções do menu principal e solicita uma escolha
+    OpcaoPrimeiroMenu = int(input('(1) Gerenciar estudantes.'
+                                '\n(2) Gerenciar professores.'
+                                '\n(3) Gerenciar disciplinas'
+                                '\n(4) Gerenciar turmas'
+                                '\n(5) Gerenciar matrículas'
+                                '\n(9) Sair'
+                                '\nInforme a opção desejada a seguir: '))
+
+    # Se a opção selecionada for sair (9), encerra o programa
+    if OpcaoPrimeiroMenu == 9:
+        print('Finalizando aplicação. Obrigado por utilizar nosso Software.')
         break
-    else:
-        print('o numero digitado não corresponde ao numeros na lista voltaremos você ao menu inicial para que solicite novamento com os numero corretos')
-        continue
-    if OpcaoPrimeiroMenu == 1 or 2 or 3 or 4 or 5:
 
-# esse while é o segundo menu onde to fazendo esse incluir etc
-        menus = 0
-        while menus!= 9:
-            menus = int(input(f'*****MENU DE {escrito}*****'
-                              '\n(1)Incluir'
-                              '\n(2)Listar'
-                              '\n(3)Atualizar'
-                              '\n(4)Excluir'
-                              '\n(9)voltar ao menu principal: '))
-            if menus == 1:
-                NomeAluno= str(input('Qual o nome a ser adicionado?'))
+    # Se a opção selecionada estiver entre 1 e 5, executa as ações correspondentes
+    if OpcaoPrimeiroMenu in [1, 2, 3, 4, 5]:
+        # Determina qual opção foi selecionada e ajusta o texto correspondente
+        if OpcaoPrimeiroMenu == 1:
+            escrito = 'ESTUDANTES'
+        elif OpcaoPrimeiroMenu == 2:
+            escrito = 'PROFESSORES'
+        elif OpcaoPrimeiroMenu == 3:
+            escrito = 'DISCIPLINAS'
+        elif OpcaoPrimeiroMenu == 4:
+            escrito = 'TURMAS'
+        elif OpcaoPrimeiroMenu == 5:
+            escrito = 'MATRÍCULAS'
+
+        # Loop para o menu secundário correspondente à opção selecionada
+        while True:
+            print(f'***** MENU DE {escrito} *****')
+            OpcaoSegundoMenu = int(input('(1) Incluir'
+                                       '\n(2) Listar'
+                                       '\n(3) Atualizar'
+                                       '\n(4) Excluir'
+                                       '\n(9) Voltar ao menu principal: '))
+
+            # Se escolher voltar (9), sai deste menu e volta ao menu principal
+            if OpcaoSegundoMenu == 9:
+                print('Voltando ao menu principal.')
+                break
+
+            # Opção para incluir um novo nome
+            if OpcaoSegundoMenu == 1:
+                NomeAluno = input('Qual o nome a ser adicionado? ')
                 nome.append(NomeAluno)
-            elif menus == 2:
-                print("-" * 15+'LISTANDO'+"-" * 15)
-                print(nome)
-            elif menus == 3:
-                print("-" * 15+'ATUALIZANDO'+"-" * 15)
-                
-            elif menus == 4:
-                print("-" * 15+'EXCUINDO'+"-" * 15)
-                
-            elif menus == 9:
-                 print ('voltando ao inicio do menu ')
-                 continue 
+
+            # Opção para listar todos os nomes adicionados
+            elif OpcaoSegundoMenu == 2:
+                print("-" * 15 + 'LISTANDO' + "-" * 15)
+                if len(nome)==0:
+                    print('não a estudantes cadastrados por favor cadastre um estudante')
+                else:
+                    for aluno in nome:
+                        print(aluno)
+
+            # Opção para atualizar um nome existente (não implementada)
+            elif OpcaoSegundoMenu == 3:
+                print("-" * 15 + 'ATUALIZANDO' + "-" * 15)
+                # Implemente a lógica de atualização aqui
+
+            # Opção para excluir um nome existente (não implementada)
+            elif OpcaoSegundoMenu == 4:
+                print("-" * 15 + 'EXCLUINDO' + "-" * 15)
+                # Implemente a lógica de exclusão aqui
+
+            # Se a opção não for válida, exibe mensagem de erro
             else:
-                print('o numero digitado não corresponde ao numeros na lista voltaremos você ao menu inicial para que solicite novamento com os numero corretos')
-print('espere um pouco estamos finalizando')
-print('Finalizando aplicação, obrigado por utilizar nosso Software')
+                print('Opção inválida. Por favor, escolha uma opção válida.')
 
-# primeiro menu basico  para eu ir em determinados menus
-OpcaoPrimeiroMenu = 0
-nome= []
-
-while OpcaoPrimeiroMenu != 9:
-    print('AGUARDE ENQUANTO O MENU É INCIADO')
-    print("-" * 15+'MENU PRINCIPAL'+"-" * 15)
-    OpcaoPrimeiroMenu = int(input('(1) Gerenciar estudantes.' 
-                     '\n(2) Gerenciar professores.'
-                     '\n(3) Gerenciar diciplinas'
-                     '\n(4) Gerenciar turmas'
-                     '\n(5) Gerenciar matricula'
-                     '\n(9) Sair'
-                     '\nInforme a opção desejada a seguir:'))
-    if OpcaoPrimeiroMenu == 1:
-        escrito= 'ESTUDANTES'
-    elif OpcaoPrimeiroMenu == 2:
-        escrito= 'PROFESSORES'
-    elif OpcaoPrimeiroMenu == 3:
-        escrito= 'DICIPLINAS'
-    elif OpcaoPrimeiroMenu == 4:
-        escrito= 'TURMAS'
-    elif OpcaoPrimeiroMenu == 5:
-        escrito= 'MATRICULAS'
-    elif OpcaoPrimeiroMenu == 9:
-        break
+    # Se a opção do menu principal não for válida, exibe mensagem de erro
     else:
-        print('o numero digitado não corresponde ao numeros na lista voltaremos você ao menu inicial para que solicite novamento com os numero corretos')
-        continue
-    if OpcaoPrimeiroMenu == 1 or 2 or 3 or 4 or 5:
-
-# esse while é o segundo menu onde to fazendo esse incluir etc
-        menus = 0
-        while menus!= 9:
-            menus = int(input(f'*****MENU DE {escrito}*****'
-                              '\n(1)Incluir'
-                              '\n(2)Listar'
-                              '\n(3)Atualizar'
-                              '\n(4)Excluir'
-                              '\n(9)voltar ao menu principal: '))
-            if menus == 1:
-                NomeAluno= str(input('Qual o nome a ser adicionado?'))
-                nome.append(NomeAluno)
-            elif menus == 2:
-                print("-" * 15+'LISTANDO'+"-" * 15)
-                print(nome)
-            elif menus == 3:
-                print("-" * 15+'ATUALIZANDO'+"-" * 15)
-                
-            elif menus == 4:
-                print("-" * 15+'EXCUINDO'+"-" * 15)
-                
-            elif menus == 9:
-                 print ('voltando ao inicio do menu ')
-                 continue 
-            else:
-                print('o numero digitado não corresponde ao numeros na lista voltaremos você ao menu inicial para que solicite novamento com os numero corretos')
-print('espere um pouco estamos finalizando')
-print('Finalizando aplicação, obrigado por utilizar nosso Software')
-
+        print('Opção inválida. Por favor, escolha uma opção válida.')
